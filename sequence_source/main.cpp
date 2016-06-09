24711a76e289123c980292f347637ee141ad59da
MinhVN <minhvo.me@gmail.com>
2016-06-10 03:36:55 +0700

    add result time to file

--- sequence_source/main.cpp
+++ sequence_source/main.cpp
@@ -9,53 +9,53 @@ using namespace std;
 
 //Global database
 vector< Transaction > database;
-vector< vector<string> > frequentSet; 
+vector< vector<string> > frequentSet;
 //set minSup
 int minSup;
 
 //function for remove all item sets lower than the minsup
 vector< vector<string> > removeAllItemSet_BelowMinSupp(
 	vector< vector<string> > itemSets){
-		
+
 	vector< vector<string> > result;
-	
+
 	for(int i = 0;i < itemSets.size();i++){
-		
+
 		vector<string> * curSet = &itemSets[i];
-		
-			
+
+
 		//Count the supp
-		
+
 		int count=0;//count of current itemset
-		
+
 		for (int j = 0;j < database.size(); j++){
-			
-			
+
+
 			//check if the current transaction contains the current itemset
 			vector<string> currentTransaction = (database[j].getItemSet());
 			bool isOk = true;//true: the transaction contains the itemset - false: the transaction doesn't contains the itemset
 			for(int k = 0; k < curSet->size(); k++){
 				bool isContain = false;
-				
+
 				for(int l = 0; l < currentTransaction.size();l++){
 					if(curSet->at(k).compare(currentTransaction.at(l))==0){
 						isContain = true;
 						break;
 					}
 				}
-				
+
 				if(!isContain){
 					isOk = false;
 					break;
 				}
 			}
-			
+
 			if(isOk && curSet->size()>0 && currentTransaction.size()>0) count++;
-			
+
 		}
-		
+
 		if(count >= minSup)
-			result.push_back(*curSet);		
+			result.push_back(*curSet);
 	}
 	return result;
 }
@@ -127,7 +127,7 @@ bool hasInFrequentSubset(const vector<string> &candidateSetKplus1, vector< vecto
 
 	//check if any k-subset doesn't exist in L(k)
 	for(int i = 0; i < (*subsets).size(); i++) {
-		
+
 		vector<string> sub = (*subsets).at(i);
 
 		bool foundSubInLK = false;
@@ -136,7 +136,7 @@ bool hasInFrequentSubset(const vector<string> &candidateSetKplus1, vector< vecto
 
 			vector<string> kItemSet = largeItemSetK.at(j);
 			if(sub == kItemSet) {
-			
+
 				foundSubInLK = true;
 				break;
 			}
@@ -144,10 +144,10 @@ bool hasInFrequentSubset(const vector<string> &candidateSetKplus1, vector< vecto
 
 		// k-subitem doest exists in Lk
 		if(foundSubInLK == false) {
-			
+
 			result = true;
 			break;
-		} 
+		}
 	}
 
 	delete subsets;
@@ -165,28 +165,28 @@ vector< vector<string> > aprioriGen(vector< vector<string> > largeItemSetK)
 	{
 		//Add to frequent set (Global)
 		frequentSet.push_back(largeItemSetK.at(i));
-		for(int j = 1; j < size; j++) 
+		for(int j = 1; j < size; j++)
 		{
 			vector<string> l1 = largeItemSetK.at(i);
 			vector<string> l2 = largeItemSetK.at(j);
 			bool haveSamePrefix = true;
-			for(int t = 0; t < (k-1); t++) 
+			for(int t = 0; t < (k-1); t++)
 			{
 				if(l1.at(t) != l2.at(t))
 				{
 					haveSamePrefix = false;
 					break;
-					
+
 				}
 			}
-			if(haveSamePrefix) 
+			if(haveSamePrefix)
 			{
-				if(l1.at(k-1).compare(l2.at(k-1)) != 0) 
+				if(l1.at(k-1).compare(l2.at(k-1)) != 0)
 				{
 					vector<string> temp = l1;
 					temp.push_back(l2.at(k-1));
 					bool isInValid = hasInFrequentSubset(temp, largeItemSetK);
-					if(!isInValid) 
+					if(!isInValid)
 					{
 						result.push_back(temp);
 					}
@@ -205,7 +205,7 @@ ostream& operator<< (ostream& out, const vector<T>& v) {
     size_t last = v.size() - 1;
     for(size_t i = 0; i < v.size(); ++i) {
         out << v[i];
-        if (i != last) 
+        if (i != last)
             out << ", ";
     }
     out << "]";
@@ -215,25 +215,28 @@ ostream& operator<< (ostream& out, const vector<T>& v) {
 
 //testing main
 int main(int argc, char* argv[]){
-	
+
 	//read database
 	database = TeamHelper::getDatabase("../datasource/10k.txt");
-	
+
 	//set minSup
 	minSup = 3;
-	
+
 	struct timeval benchmark;
-	gettimeofday(&benchmark, NULL);	
+	gettimeofday(&benchmark, NULL);
 	long startTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
-	//to find frequent item set
+	// //to find frequent item set
 	vector< vector<string> > L1 = getL1FromDatabase();
 	while(L1.size() > 0)
 	{
 		L1 = aprioriGen(L1);
-	}	
+	}
 	gettimeofday(&benchmark, NULL);
 	long endTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
-	
-	cout<<"Time to run: "<<endTime - startTime<<"ms";
+	ofstream myfile;
+  myfile.open ("result_5k.txt");
+  myfile << (endTime - startTime) << "ms\n";
+  myfile.close();
+	cout<<"Time to run: "<< endTime - startTime<<"ms";
 	return 0;
 }
