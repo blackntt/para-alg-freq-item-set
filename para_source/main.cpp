ebf70e8aa57be9ff7f9cad807b1aff903651e750
black <blackntt@gmail.com>
2016-06-19 08:52:12 +0700

    update main.cpp of parallel for testing

--- para_source/main.cpp
+++ para_source/main.cpp
@@ -104,44 +104,15 @@ vector< vector<string> > removeAllItemSet_BelowMinSupp(
 }
 
 //DUCDM create large item set with one element
-void getL1Parallel(vector< Transaction > db, vector <string> &result)
-{
-	for (int j = 0; j < db.size(); j++)
-	{
-		vector<string> trans = (db.at(j).getItemSet());
-		for(int i = 0; i < trans.size(); i++)
-		{
-			result.push_back(trans.at(i));
-		}
-	}
-}
-//DUCDM create large item set with one element
 vector< vector<string> > getL1FromDatabase()
 {
-	int subSize = database.size()/numOfCPU;
-	vector<Transaction> dataSubSets[numOfCPU];
-	vector <string> results[numOfCPU];
-	int j=0;
-
-	for(int i=0;i<numOfCPU;i++){
-		int curMaxIndex = i*subSize+subSize;
-		if(curMaxIndex < database.size())
-			curMaxIndex = database.size();
-		for(;j<curMaxIndex;j++)
-			dataSubSets[i].push_back(database.at(j));
-	}
-	vector< thread > threads;
-	for(int i=0; i<numOfCPU; i++){
-		threads.push_back(thread(&getL1Parallel, dataSubSets[i], std::ref(results[i])));
-	}
-
-	for(int i=0;i<numOfCPU;i++){
-		threads[i].join();
-	}
 	set <string> C1;
-	for(int i=0;i<numOfCPU;i++){
-		for(j=0;j<results[i].size();j++){
-			C1.insert((results[i]).at(j));
+	for (int j = 0; j < database.size(); j++)
+	{
+		vector<string> trans = (database[j].getItemSet());
+		for(int i = 0; i < trans.size(); i++)
+		{
+			C1.insert(trans.at(i));
 		}
 	}
 	vector< vector<string> > c1Vector;
@@ -237,7 +208,7 @@ vector< vector<string> > aprioriGen(vector< vector<string> > largeItemSetK)
 	{
 		//Add to frequent set (Global)
 		frequentSet.push_back(largeItemSetK.at(i));
-		for(int j = i+1; j < size; j++)
+		for(int j = i+1; j < size; j++)//fix loop
 		{
 			vector<string> l1 = largeItemSetK.at(i);
 			vector<string> l2 = largeItemSetK.at(j);
@@ -284,34 +255,31 @@ ostream& operator<< (ostream& out, const vector<T>& v) {
     return out;
 }
 
+
 //testing main
 int main(int argc, char* argv[]){
-	for (int r = 8; r <= 10; r++) {
-    //read database
-    string s_r = to_string(r);
-    string file_name_input = "../datasource/" + s_r + ".txt";
-    string file_name_output = s_r + "_result.txt";
-    cout<< file_name_output << endl;
-    database = TeamHelper::getDatabase(file_name_input);
 
-		//set minSup
-		minSup = 3;
+	//read database
+	database = TeamHelper::getDatabase("../datasource/test.txt");
 
-		struct timeval benchmark;
-		gettimeofday(&benchmark, NULL);
-		long startTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
-		//to find frequent item set
-		vector< vector<string> > L1 = getL1FromDatabase();
-		 while(L1.size() > 0)
-		 {
-		 	L1 = aprioriGen(L1);
-		 }
-		gettimeofday(&benchmark, NULL);
-		long endTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
-		ofstream outFile;
-    outFile.open (file_name_output.c_str());
-    outFile << (endTime - startTime) <<"ms";
-    outFile.close();
-	}
+	//set minSup
+	minSup = 3;
+
+	struct timeval benchmark;
+	gettimeofday(&benchmark, NULL);
+	long startTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
+	// //to find frequent item set
+	vector< vector<string> > L1 = getL1FromDatabase();
+	 while(L1.size() > 0)
+	 {
+	 	L1 = aprioriGen(L1);
+	 }
+	gettimeofday(&benchmark, NULL);
+	long endTime = benchmark.tv_sec * 1000 + benchmark.tv_usec / 1000;
+	ofstream outFile;
+	outFile.open ("result_10k.txt");
+	outFile << (endTime - startTime) <<"ms";
+	outFile.close();
+	cout<<"Time to run: "<<endTime - startTime<<"ms" << endl;
 	return 0;
 }
